59dd866180855fde300f684cd576bf39be4e8e45
deepakBhalode <deepak.bhalode.sbg@gmail.com>
2023-12-18 22:43:09 +0530

    audio player component added

--- kiddo-new-year-app/src/app/components/audio-player/audio-player.component.ts
+++ kiddo-new-year-app/src/app/components/audio-player/audio-player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { audiosMetadata } from 'src/app/shared/resources/audio-metadata'
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-audio-player',
@@ -7,9 +9,122 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AudioPlayerComponent implements OnInit {
 
+  audioNumber = 0
+  isAudioPlaying = false
+  isAudioPaused = false
+  isAudioPlayed = true
+  currentAudioTime = 0
+  currentAudioTimeInMmSs = "0:00"
+  audioProgressValue = 0
+  currentAudio = new Audio()
+  audio_details = audiosMetadata
+  audioId = this.audio_details[this.audioNumber]['songId']
+  audioImage = this.audio_details[this.audioNumber]['coverImg']
+  audioName = this.audio_details[this.audioNumber]['name']
+  artistName = this.audio_details[this.audioNumber]['artistName']
+  audioDuration = this.audio_details[this.audioNumber]['duration']
+
   constructor() { }
 
   ngOnInit(): void {
+    //this.openMusicLoaderDialog()
   }
+
+  openMusicLoaderDialog() {
+    swal.fire({
+      icon: "success",
+      color: "#716add",
+      timer: 5000,
+      timerProgressBar: true,
+      showConfirmButton: false,
+      width: 330,
+      html: `
+        <div style="text-align:center;">
+          <h1><b>Hold for sec</b></h1>
+          Let me ready the required setup!
+        </div>
+      `,
+    });
+  }
+
+  handleAudioPlayPause() {
+    if (this.isAudioPlaying == false && this.currentAudio.paused == true) {
+      this.playAudio()
+    }
+    else {
+      this.pauseAudio()
+    }
+  }
+
+  playAudio() {
+      this.audioProgressValue = 0
+      this.currentAudioTimeInMmSs = "0:00"
+      this.isAudioPlaying = true
+      this.isAudioPlayed = false
+      this.isAudioPaused = true
+      this.setAudioDetails();
+      this.currentAudio.load();
+      this.currentAudio.play();
+      this.handleAudioProgress();
+  }
+
+  setAudioDetails() {
+    this.audioId = this.audio_details[this.audioNumber]['songId']
+    this.artistName = this.audio_details[this.audioNumber]['artistName']
+    this.audioName = this.audio_details[this.audioNumber]['name']
+    this.audioImage = this.audio_details[this.audioNumber]['coverImg']
+    this.audioDuration = this.audio_details[this.audioNumber]['duration']
+    this.currentAudio.src = this.audio_details[this.audioNumber]['url']
+    this.currentAudio.currentTime = this.currentAudioTime;
+  }
+
+  handleAudioProgress() {
+    this.currentAudio.ontimeupdate = () => {
+      let minutes = Math.floor(this.currentAudio.currentTime / 60);
+      let seconds = "0" +  Math.floor(this.currentAudio.currentTime - minutes * 60);
+      this.currentAudioTimeInMmSs = minutes.toString().substr(-2) + "." + seconds.substr(-2);
+
+      let progressPercent = parseFloat(this.currentAudioTimeInMmSs)/parseFloat(this.audioDuration.replace(':', '.'))
+      this.audioProgressValue = parseInt((progressPercent*100).toFixed(0))
+      this.currentAudioTimeInMmSs = this.currentAudioTimeInMmSs.replace('.', ':')
+
+      this.currentAudio.onended = () => {
+        console.log("Audio ended.")
+        this.playNextAudio()
+      }
+    }
+  }
+
+  pauseAudio() {
+    this.isAudioPlayed = true
+    this.isAudioPaused = false
+    this.isAudioPlaying = false
+    this.currentAudio.pause();
+    this.currentAudioTime = this.currentAudio.currentTime;
+  }
+
+  playPreviousAudio() {
+    this.currentAudioTime = 0
+    if(this.audioNumber == 0) {
+      this.audioNumber = 4
+    }
+    else {
+      this.audioNumber = this.audioNumber - 1
+    }
+    this.playAudio()
+  }
+
+  playNextAudio() {
+    this.currentAudioTime = 0
+    if(this.audioNumber == 4) {
+      this.audioNumber = 0
+    }
+    else {
+      this.audioNumber = this.audioNumber + 1
+    }
+    this.playAudio()
+  }
+
+
 
 }
---
+++ kiddo-new-year-app/src/app/shared/resources/audio-metadata.ts
@@ -0,0 +1,42 @@
+export const audiosMetadata = [
+  {
+    songId: "1",
+    name: "Chalo Chalein",
+    coverImg: "../assets/images/chalo-chalein-img.png",
+    artistName: "The Starzone",
+    duration: "4:45",
+    url:"../assets/audios/Chalo-Chalein.mp3"
+  },
+  {
+    songId: "2",
+    name: "A Thousand Years",
+    coverImg: "../assets/images/thousand-years-img.png",
+    artistName: "Christina Perri",
+    duration: "4:47",
+    url:"../assets/audios/A-Thousand-Years.mp3"
+  },
+  {
+    songId: "3",
+    name: "Saari Ki Saari",
+    coverImg: "../assets/images/sari-ki-sari-img.png",
+    artistName: "Darshan Raval",
+    duration: "4:06",
+    url:"../assets/audios/Saari-Ki-Saari.mp3"
+  },
+  {
+    songId: "4",
+    name: "Parda",
+    coverImg: "../assets/images/parda-img.png",
+    artistName: "Deepak Rathore Project",
+    duration: "4:23",
+    url:"../assets/audios/Parda.mp3"
+  },
+  {
+    songId: "5",
+    name: "If the world was ending",
+    coverImg: "../assets/images/if-the-world-img.png",
+    artistName: "JP Saxe & Julia Michaels",
+    duration: "3:28",
+    url:"../assets/audios/If-The-World-Was-Ending.mp3"
+  }
+]
